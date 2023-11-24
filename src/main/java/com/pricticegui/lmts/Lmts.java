e6a06d95ac3bcb6a9b805c0fdb297849e26afbef
Sudip Tamang <sudiptamang000@gmail.com>
2023-11-24 22:26:00 +0545

    initial commit

---
+++ src/main/java/com/pricticegui/lmts/Lmts.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.pricticegui.lmts;
+
+import javax.swing.SwingUtilities;
+
+/**
+ *
+ * @author sudip
+ */
+public class Lmts {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new MainJFrame().setVisible(true);
+        });
+    }
+}
